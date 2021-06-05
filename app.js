dfb2bdf3c9de9cb882e4bf5e14df5b519ba88914
Charis <bluegrassgal222@gmail.com>
2021-06-04 23:50:45 -0600

    hide api keys and phone no

--- app.js
+++ app.js
@@ -1,39 +1,35 @@
 axios = require("axios");
+const { WX_KEY, SMS_KEY, PHONE_NO } = require("./api_keys");
+
+// weather
+
+const wxRequest = {
+  method: 'GET',
+  url: `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=2019-06-13T00:00:00&endDateTime=2019-06-20T00:00:00&unitGroup=uk&contentType=json&dayStartTime=0:0:00&dayEndTime=0:0:00&location=Enid,OK,US&key=${WX_KEY}`,
+}
+
+axios.request(wxRequest)
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error));
+
+
+// sms
 
 const options = {
   method: 'POST',
   url: 'https://sms77io.p.rapidapi.com/sms',
   headers: {
     'content-type': 'application/x-www-form-urlencoded',
-    'x-rapidapi-key': '9b1a11cba5msh3649975e0b88403p1d2db5jsnc3351d6c877e',
+    'x-rapidapi-key': SMS_KEY,
     'x-rapidapi-host': 'sms77io.p.rapidapi.com'
   },
   data: {
-    to: '+7703318612',
-    p: '9b1a11cba5msh3649975e0b88403p1d2db5jsnc3351d6c877e',
+    to: PHONE_NO,
+    p: SMS_KEY,
     text: 'Hello Charis :)'
   }
 };
 
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-// weather
-
-// P9XNPPBGVPQ8MNQDRMPN6PLUE
-
-const wxRequest = {
-  method: 'GET',
-  url: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=2019-06-13T00:00:00&endDateTime=2019-06-20T00:00:00&unitGroup=uk&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=Sterling,VA,US&key=P9XNPPBGVPQ8MNQDRMPN6PLUE',
-}
-
-axios.request(wxRequest).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+axios.request(options)
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error));
