6bf1294e8ada6d4ba4646fbfcf75a6685c4c1845
Charis <bluegrassgal222@gmail.com>
2021-06-05 00:56:14 -0600

    move phone number into secrets

--- app.js
+++ app.js
@@ -15,23 +15,13 @@ async function checkWX(){
   /* SmtpJS.com - v3.0.0 */
   var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
   
-  // format: '7703318612@txt.att.net'
-  // Alltel @message.alltel.com 
-  // AT&T @txt.att.net 
-  // Nextel @messaging.nextel.com 
-  // Sprint @messaging.sprintpcs.com 
-  // SunCom @tms.suncom.com 
-  // T-mobile @tmomail.net 
-  // VoiceStream @voicestream.net 
-  // Verizon @vtext.com (text only) @vzwpix.com (pictures and videos)
-  
   function sendText() {
     
     Email.send({
       Host: "smtp.gmail.com",
       Username: "alignbank@gmail.com",
       Password: "UnsafePassword",
-      To: `${PHONE_NO}@vtext.com`,
+      To: PHONE_NO,
       From: "alignbank@gmail.com",
       Subject: "",
       'MIME-Version': '1.0rn',
