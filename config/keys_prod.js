9f2b48ee18c6b514ada7d3cfbbad269d0dd11336
Charis <bluegrassgal222@gmail.com>
2021-06-18 12:39:17 -0400

    add keys for mom and dad

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,7 @@ module.exports = {
   WX_KEY: process.env.WX_KEY,
   CHARIS_NO: process.env.CHARIS_NO,
   RYAN_NO: process.env.RYAN_NO,
+  MOM_MORSE_NO: process.env.MOM_MORSE_NO,
+  DAD_MORSE_NO: process.env.DAD_MORSE_NO,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
 }
